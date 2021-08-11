edf85a086736d4dd2559aff01312a5979428078f
Can Kurtur <>
2021-08-11 21:35:04 +0300

    [MA-0] UIColor and UIView extensions created.

---
+++ movieApp/movieApp/Utility/Extensions/UIColorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColorExtension.swift
+//  movieApp
+//
+//  Created by Can Kurtur on 9.08.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    // MARK: - Properties
+    static let almostBlack = Colors.almostBlack
+    static let lightGreyBlue30 = Colors.lightGreyBlue30
+    static let vibrantBlue = Colors.vibrantBlue
+    static let whiteTwo = Colors.whiteTwo
+    static let electricBlue = Colors.electricBlue
+    static let white = Colors.white
+
+    // MARK: - Business Logic
+    static func getColor(colorName: String) -> UIColor {
+        guard let color = UIColor(named: colorName) else {
+            return UIColor.white
+        }
+        return color
+    }
+}
