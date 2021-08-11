3899505e284d9cbd98440707d37c8d4f2dcfc73f
Can Kurtur <>
2021-08-11 21:35:56 +0300

    [MA-0] Colors class created.

---
+++ movieApp/movieApp/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  movieApp
+//
+//  Created by Can Kurtur on 9.08.2021.
+//
+
+import UIKit
+
+class Colors {
+    static let almostBlack: UIColor = UIColor.getColor(colorName: "almostBlack")
+    static let lightGreyBlue30: UIColor = UIColor.getColor(colorName: "lightGreyBlue30")
+    static let vibrantBlue: UIColor = UIColor.getColor(colorName: "vibrantBlue")
+    static let whiteTwo: UIColor = UIColor.getColor(colorName: "whiteTwo")
+    static let electricBlue: UIColor = UIColor.getColor(colorName: "electricBlue")
+    static let white: UIColor = UIColor.getColor(colorName: "white")
+}
